fceb6910451c99da29c22b0289d45975d5801276
jpan <j.pan.0923551124@gmail.com>
2021-12-30 13:03:50 +0800

    second commit

--- app/src/main/java/com/pan/mywebapp/MainActivity_two.kt
+++ app/src/main/java/com/pan/mywebapp/MainActivity_two.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity_two : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_two)
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
             startActivity(Intent(this,SecondActivity::class.java))
